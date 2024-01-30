f16e2f25aae6077ea8da0ff396ca10ab3ddab033
Davide Del Papa <davidedelpapa@gmail.com>
2024-01-30 17:00:11 +0100

    Added example GET route with parameter

--- Program.vb
+++ Program.vb
@@ -4,10 +4,14 @@ Imports Microsoft.AspNetCore.Http
 
 Module Program
     Sub Main(args As String())
+        Dim host = "http://localhost:5000"
+
         Dim builder = WebApplication.CreateBuilder(args)
         Dim app = builder.Build()
+
+        app.MapGet("/hello/{username}", Function(username as String) "Hello "& username &"!")
         app.MapGet("/", Function() "Hello World!")
 
-        app.Run("http://localhost:5000")
+        app.Run(host)
     End Sub
 End Module
